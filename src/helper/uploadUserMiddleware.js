f316acca4acdbd1999a1a9242c7353971d1ec96b
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-03-11 23:29:25 +0700

    14. Add Multter upload image in users_detail

--- src/helper/uploadUserMiddleware.js
+++ src/helper/uploadUserMiddleware.js
@@ -1,30 +1,29 @@
 const multer = require('multer');
-const path = require("path");
+const path = require ('path');
 
 const storage = multer.diskStorage({
     destination:(req,file,callback)=>{
-        callback(null, './public/category_image');
+        callback (null,'./public/user_image');
     },
-
     filename:(req,file,callback)=>{
-        const nameFormat =
-        `${Date.now()}-${file.fieldname}${path.extname(
+        const nameFormat =`${Date.now()}-${file.fieldname}${path.extname(
             file.originalname
         )}`;
         callback(null,nameFormat)
     }
 })
+
 const upload = multer({
     storage:storage,
     limits:2*1000*1000
 })
 
-const singleUpload= (req,res,next) =>{
-    const uploadSingle= upload.single("category_cover");
+const singleUpload= (req,res,next)=>{
+    const uploadSingle = upload.single("users_foto");
     uploadSingle(req,res,(err)=>{
         if(err){
             res.status(500).send({
-                msg:"mullter error",
+                msg:"mullter Eroor",
                 error:err
             })
         }else{
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,10 @@
 const usersRoutes = require('express').Router()
 const usersConttroler = require('../controllers/detailuserController')
 const authMiddleware = require ('../helper/authMiddelware')
+const uploadMiddleware = require('../helper/uploadUserMiddleware')
 
 usersRoutes.get('/',usersConttroler.viewProfileDetail)
-usersRoutes.post('/',authMiddleware.checkLogin,usersConttroler.createDetailUser)
+usersRoutes.post('/',authMiddleware.checkLogin,uploadMiddleware,usersConttroler.createDetailUser)
 usersRoutes.get('/all', usersConttroler.viewUser)
-
-
 
 module.exports=usersRoutes
