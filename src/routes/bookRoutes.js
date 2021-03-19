f94835245e2ffbc90df442c21752d50fb4557b6c
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-03-20 06:39:06 +0700

    20.ROlling Routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,7 +5,7 @@ const uploadMidleware = require('../helper/uploadMiddleware')
 
 bookRoutes.get('/show',bookConttroler.getBooks)
 bookRoutes.get('/',authMiddleware.checkLogin,bookConttroler.getBooksByUser)
-bookRoutes.post('/users',uploadMidleware,authMiddleware.checkLogin,bookConttroler.createbooksUsers)
+bookRoutes.post('/users',authMiddleware.checkLogin,uploadMidleware,bookConttroler.createbooksUsers)
 bookRoutes.delete('/:id',authMiddleware.checkLogin,bookConttroler.deleteBooks)
 bookRoutes.put('/:id',authMiddleware.checkLogin,bookConttroler.updateBooks)
 bookRoutes.get('/:id',authMiddleware.checkLogin,bookConttroler.getBookById)
