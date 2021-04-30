164d42720dcdc47cc9a98260b748f2eeeb348b67
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-05-01 02:16:02 +0700

    27. multter error

--- src/helper/uploadMiddleware.js
+++ src/helper/uploadMiddleware.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 const storage = multer.diskStorage({
     destination: (req,file,callback)=>{
-        callback(null, './public');
+        callback(null, './public/book_image');
     },
 
     filename:(req,file,callback)=>{
